6b90ab6ea495c6d3fe652c9c92e47d032221eb16
.jp. <152410055+jpmellow@users.noreply.github.com>
2024-04-28 18:03:09 +0000

    Completed base functionality.

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,5 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
@@ -29,52 +7,62 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let socialMedia = "";
+  if (variables.socialMediaPosition === "position-left") {
+    socialMedia = `<ul class="position-left">
+      <li><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+      <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
+      <li><a href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+      <li><a href="${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+    </ul>`;
+  } else {
+    socialMedia = `<ul class="position-right">
+      <li><a href="${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+      <li><a href="${variables.github}"><i class="fab fa-github"></i></a></li>
+      <li><a href="${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+      <li><a href="${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+    </ul>`;
+  }
+
+  const name = variables.name ? variables.name : "";
+  const lastName = variables.lastName ? variables.lastName : "";
+  const role = variables.role ? variables.role : "";
+  const country = variables.country ? variables.country : "";
+  const city = variables.city ? variables.city : "";
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+    ${cover}
+    <img src="${variables.avatarURL}" class="photo" />
+    <h1>${name} ${lastName}</h1>
+    <h2>${role}</h2>
+    <h3>${city}, ${country}</h3>
+    ${socialMedia}
+  </div>`;
 }
 
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should show the cover image
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
     socialMediaPosition: "position-left",
-    // social media usernames
+
     twitter: null,
     github: null,
     linkedin: null,
     instagram: null,
+
     name: null,
     lastName: null,
     role: null,
     country: null,
-    city: null
+    city: null,
   };
   render(window.variables); // render the card for the first time
 
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
       const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
       let values = {};
       values[attribute] =
